a43d0a0f9b18424ab69c301825a003634f84dedc
CJ Gladback <cjgladback@gmail.com>
2018-04-21 19:15:36 -0400

    Generic
    Many failures were had; here's the few copy-pasta elements that continued to function after my touch.

--- ld41/Assets/Scripts/fieldOfView.cs
+++ ld41/Assets/Scripts/fieldOfView.cs
@@ -1,16 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class fieldOfView : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+	public float fovangle = 110f;
+	public bool playerInSight;
+	public Vector3 personalLastSighting;
+
+	private NavMeshAgent nav;
+	private CapsuleCollider col;
+	private Animator anim;
+	//private LastPlayerSighting lastPlayerSighting;
+	private GameObject player;
+	private Animator playerAnim;
+	//private HashIDs hash;
+	private Vector3 previousSighting;
+
+	void Awake(){
+		nav = GetComponent<NavMeshAgent> ();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
 }
